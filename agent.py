0080a8b6cbe0d8a08e54ce276f2c328abb4c99e8
Aaron Marquez <aamarq5@msn.com>
2018-02-25 16:54:06 -0800

    Change Q-table update logic
    Changed Q-table update equation, set random seeds, and cleaned up code

--- agent.py
+++ agent.py
@@ -1,4 +1,8 @@
+#!/usr/bin/env python
+
 import random
+import pandas as pd
+import numpy as np
 
 
 class AgentBasic(object):
@@ -24,24 +28,43 @@ class AgentRandom(object):
 
 class AgentLearning(object):
     '''' Agent that can learn via Q-learning. '''
-    def __init__(self, env, alpha=0.1, epsilon=1.0):
+    def __init__(self, env, alpha=0.1, epsilon=1.0, gamma=0.9):
         self.env = env
-        self.alpha = alpha          # learning factor
+        self.alpha = alpha          # Learning factor
         self.epsilon = epsilon
+        self.gamma = gamma          # Discount factor
         self.Q_table = dict()
+        self._set_seed()
         # Following variables for statistics
         self.training_trials = 0
         self.testing_trials = 0
+
+    def _set_seed(self):
+        ''' Set random seeds for reproducibility. '''
+        np.random.seed(21)
+        random.seed(21)
+
+    def build_state(self, features):
+        ''' Build state by concatenating features (bins) into 4 digit int. '''
+        return int("".join(map(lambda feature: str(int(feature)), features)))
 
     def create_state(self, obs):
         ''' Create state variable from observation.
 
         Args:
-            obs: Observation list.
+            obs: Observation list with format [horizontal position, velocity,
+                 angle of pole, angular velocity].
         Returns:
             state: State tuple
         '''
-        state = tuple([state < 0.0 for state in obs])
+        cart_position_bins = pd.cut([-2.4, 2.4], bins=10, retbins=True)[1][1:-1]
+        pole_angle_bins = pd.cut([-2, 2], bins=10, retbins=True)[1][1:-1]
+        cart_velocity_bins = pd.cut([-1, 1], bins=10, retbins=True)[1][1:-1]
+        angle_rate_bins = pd.cut([-3.5, 3.5], bins=10, retbins=True)[1][1:-1]
+        state = self.build_state([np.digitize(x=[obs[0]], bins=cart_position_bins)[0],
+                                 np.digitize(x=[obs[1]], bins=pole_angle_bins)[0],
+                                 np.digitize(x=[obs[2]], bins=cart_velocity_bins)[0],
+                                 np.digitize(x=[obs[3]], bins=angle_rate_bins)[0]])
         return state
 
     def choose_action(self, state):
@@ -55,7 +78,8 @@ class AgentLearning(object):
         if random.random() < self.epsilon:
             action = self.env.action_space.sample()
         else:
-            max_Q = self.get_maxQ(state)                    # Find max Q value
+            # Find max Q value
+            max_Q = self.get_maxQ(state)
             actions = []
             for key, value in self.Q_table[state].items():
                 if value == max_Q:
@@ -89,9 +113,18 @@ class AgentLearning(object):
         maxQ = max(self.Q_table[state].values())
         return maxQ
 
-    def learn(self, state, action, reward):
-        ''' Update the Q-values '''
-        # Q_table[state][action] = (1 - alpha) * Q_table[state][action] + (alpha * (reward + (gamma * max(Q_table[next_state][all_actions]))))
-        # Disregarding gamma for now...
-        self.Q_table[state][action] = (1 - self.alpha) * self.Q_table[state][action] + (self.alpha * reward)
+    def learn(self, state, action, prev_reward, prev_state, prev_action):
+        ''' Update the Q-values
+
+        Args:
+            state: State at current time step.
+            action: Action at current time step.
+            prev_reward: Reward at previous time step.
+            prev_state: State at previous time step.
+            prev_action: Action at previous time step.
+        '''
+        # Updating previous state/action pair so I can use the 'future state'
+        self.Q_table[prev_state][prev_action] = (1 - self.alpha) * \
+            self.Q_table[prev_state][prev_action] + self.alpha * \
+            (prev_reward + (self.gamma * self.get_maxQ(state)))
         return
